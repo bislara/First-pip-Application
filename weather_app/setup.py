4cae8ddd0d3771a9dfc8d7b668af4b820c1fdca7
bislara <biswajeetsahoo54@gmail.com>
2019-09-05 00:11:03 +0530

    done getting the coordinate from API

--- weather_app/setup.py
+++ weather_app/setup.py
@@ -1,18 +1,9 @@
 import setuptools
 
 setuptools.setup(
-     name='weather_app_nitr',  
+     name='wthr', 
      version='0.1',
-     scripts=['weather_info'] ,
-     author="Biswajeet_Sahoo",
-     author_email="biswajeetsahoo54@gmail.com",
-     description="A Test package",
-     url="https://github.com/bislara/First-pip-Application/tree/master/weather_app",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
+     scripts=['weather_info']
  )
 
 
